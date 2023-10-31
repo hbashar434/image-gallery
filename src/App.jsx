a58d7cbc66beb7601d05c36f01ce0c024293502a
hbashar434 <h.bashar434@gmail.com>
2023-10-31 19:33:43 +0600

    initial

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="text-center text-3xl font-bold text-indigo-700">
+      Welcome to Image Gallery!
+    </div>
+  );
+};
+
+export default App;
